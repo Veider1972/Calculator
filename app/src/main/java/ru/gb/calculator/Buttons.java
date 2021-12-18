68693d5375f1aba61e21eeb7a718dbb1b47cb70a
Корховой Вадим <veider@inbox.ru>
2021-12-18 22:38:05 +0300

    Состояние на 18.12.2021. Интерфейс переработан в Material Design, добавлена ночная тема, добавлен интерфейс Parcelable, исправлена математика, удалены ресурсы на растровые образы кнопок.

--- app/src/main/java/ru/gb/calculator/Buttons.java
+++ app/src/main/java/ru/gb/calculator/Buttons.java
@@ -3,13 +3,13 @@ package ru.gb.calculator;
 public enum Buttons {
     ZERO("0"),ONE("1"),TWO("2"),THREE("3"),FOUR("4"),FIVE("5"),
     SIX("6"),SEVEN("7"),EIGHT("8"),NINE("9"),
-    DOT(null),NEGATIVE(null),
+    DOT("."),NEGATIVE("-"),
     RESET(null),
     PUT_MEMORY(null), GET_MEMORY(null),
-    PLUS(null),MINUS(null),MULTIPLY(null),DIVIDE(null),
-    EQUAL(null);
+    PLUS("+"),MINUS("-"),MULTIPLY("*"),DIVIDE("/"),
+    EQUAL("=");
 
-    String value;
+    private final String value;
 
     public String getValue() {
         return value;
--- app/src/main/java/ru/gb/calculator/MainActivity.java
+++ app/src/main/java/ru/gb/calculator/MainActivity.java
@@ -1,36 +1,28 @@
 package ru.gb.calculator;
 
-import static ru.gb.calculator.R.drawable.button_square_blue;
-import static ru.gb.calculator.R.drawable.button_square_blue_pressed;
-import static ru.gb.calculator.R.drawable.button_square_green;
-import static ru.gb.calculator.R.drawable.button_square_green_pressed;
-import static ru.gb.calculator.R.drawable.button_square_grey;
-import static ru.gb.calculator.R.drawable.button_square_grey_pressed;
-import static ru.gb.calculator.R.drawable.button_square_red;
-import static ru.gb.calculator.R.drawable.button_square_red_pressed;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button_0, button_1, button_2, button_3, button_4, button_5,
-            button_6, button_7, button_8, button_9,
-            button_dot, button_negative,
-            button_reset,
-            button_put_in_memory, button_get_from_memory,
-            button_plus, button_minus, button_multiply, button_divide,
-            button_equal;
-    TextView display;
-    static Calculator calculator;
+    MaterialButton button_0, button_1, button_2, button_3, button_4, button_5,
+                   button_6, button_7, button_8, button_9,
+                   button_dot, button_negative,
+                   button_reset,
+                   button_put_in_memory, button_get_from_memory,
+                   button_plus, button_minus, button_multiply, button_divide,
+                   button_equal;
+    MaterialTextView display;
+    private Calculator calculator;
 
-    private final static String DISPLAY_TEXT = "DISPLAY_TEXT";
+    private static final String DISPLAY_TEXT = "DISPLAY_TEXT";
+    public static final String CALCULATOR = "CALCULATOR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,57 +30,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (calculator == null) calculator = new Calculator(this);
 
-        getSupportActionBar().hide(); // скрыть заголовок окна
-
         display = findViewById(R.id.display);
 
-        button_0 = initializeButton(R.id.button_0, Buttons.ZERO, button_square_grey_pressed, button_square_grey);
-        button_1 = initializeButton(R.id.button_1, Buttons.ONE, button_square_grey_pressed, button_square_grey);
-        button_2 = initializeButton(R.id.button_2, Buttons.TWO, button_square_grey_pressed, button_square_grey);
-        button_3 = initializeButton(R.id.button_3, Buttons.THREE, button_square_grey_pressed, button_square_grey);
-        button_4 = initializeButton(R.id.button_4, Buttons.FOUR, button_square_grey_pressed, button_square_grey);
-        button_5 = initializeButton(R.id.button_5, Buttons.FIVE, button_square_grey_pressed, button_square_grey);
-        button_6 = initializeButton(R.id.button_6, Buttons.SIX, button_square_grey_pressed, button_square_grey);
-        button_7 = initializeButton(R.id.button_7, Buttons.SEVEN, button_square_grey_pressed, button_square_grey);
-        button_8 = initializeButton(R.id.button_8, Buttons.EIGHT, button_square_grey_pressed, button_square_grey);
-        button_9 = initializeButton(R.id.button_9, Buttons.NEGATIVE, button_square_grey_pressed, button_square_grey);
-        button_dot = initializeButton(R.id.button_dot, Buttons.DOT, button_square_grey_pressed, button_square_grey);
-        button_negative = initializeButton(R.id.button_negative, Buttons.NEGATIVE, button_square_grey_pressed, button_square_grey);
-        button_reset = initializeButton(R.id.button_reset, Buttons.RESET, button_square_red_pressed, button_square_red);
-        button_put_in_memory = initializeButton(R.id.button_put_in_memory, Buttons.PUT_MEMORY, button_square_green_pressed, button_square_green);
-        button_get_from_memory = initializeButton(R.id.button_get_from_memory, Buttons.GET_MEMORY, button_square_green_pressed, button_square_green);
-        button_plus = initializeButton(R.id.button_plus, Buttons.PLUS, button_square_blue_pressed, button_square_blue);
-        button_minus = initializeButton(R.id.button_minus, Buttons.MINUS, button_square_blue_pressed, button_square_blue);
-        button_multiply = initializeButton(R.id.button_multiply, Buttons.MULTIPLY, button_square_blue_pressed, button_square_blue);
-        button_divide = initializeButton(R.id.button_divide, Buttons.DIVIDE, button_square_blue_pressed, button_square_blue);
-        button_equal = initializeButton(R.id.button_equal, Buttons.EQUAL, button_square_red_pressed, button_square_red);
+        button_0 = initializeButton(R.id.button_0, Buttons.ZERO);
+        button_1 = initializeButton(R.id.button_1, Buttons.ONE);
+        button_2 = initializeButton(R.id.button_2, Buttons.TWO);
+        button_3 = initializeButton(R.id.button_3, Buttons.THREE);
+        button_4 = initializeButton(R.id.button_4, Buttons.FOUR);
+        button_5 = initializeButton(R.id.button_5, Buttons.FIVE);
+        button_6 = initializeButton(R.id.button_6, Buttons.SIX);
+        button_7 = initializeButton(R.id.button_7, Buttons.SEVEN);
+        button_8 = initializeButton(R.id.button_8, Buttons.EIGHT);
+        button_9 = initializeButton(R.id.button_9, Buttons.NINE);
+        button_dot = initializeButton(R.id.button_dot, Buttons.DOT);
+        button_negative = initializeButton(R.id.button_negative, Buttons.NEGATIVE);
+        button_reset = initializeButton(R.id.button_reset, Buttons.RESET);
+        button_put_in_memory = initializeButton(R.id.button_put_in_memory, Buttons.PUT_MEMORY);
+        button_get_from_memory = initializeButton(R.id.button_get_from_memory, Buttons.GET_MEMORY);
+        button_plus = initializeButton(R.id.button_plus, Buttons.PLUS);
+        button_minus = initializeButton(R.id.button_minus, Buttons.MINUS);
+        button_multiply = initializeButton(R.id.button_multiply, Buttons.MULTIPLY);
+        button_divide = initializeButton(R.id.button_divide, Buttons.DIVIDE);
+        button_equal = initializeButton(R.id.button_equal, Buttons.EQUAL);
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    private Button initializeButton(int button_id, Buttons button_value, int pressed_image, int unpressed_image) {
-        Button button = findViewById(button_id);
-        button.setOnTouchListener((v, event) -> onTouchListener(button, event, pressed_image, unpressed_image));
+    private MaterialButton initializeButton(int button_id, Buttons button_value) {
+        MaterialButton button = findViewById(button_id);
         button.setOnClickListener(v -> display.setText(calculator.put(button_value)));
         return button;
-    }
-
-    public boolean onTouchListener(Button button, MotionEvent event, int pressed_image, int unpressed_image) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
-            button.setBackgroundResource(pressed_image);
-        if (event.getAction() == MotionEvent.ACTION_UP)
-            button.setBackgroundResource(unpressed_image);
-        return false;
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        calculator = savedInstanceState.getParcelable(CALCULATOR);
+        calculator.setActivity(this);
         display.setText(savedInstanceState.getString(DISPLAY_TEXT));
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putParcelable(CALCULATOR,calculator);
         outState.putString(DISPLAY_TEXT, display.getText().toString());
     }
 }
